d709859e789890b8a8fbffed25a5bea4870a78fa
SeoMooseong <tjantjd26@naver.com>
2020-01-31 00:59:27 +0900

    [ 기능 : 시험 응시자, 합격자 수 parsing ]
    시험 응시자 수와 합격자 수를 파싱하기 위한 CertificateRegCntParser 구현

---
+++ src/main/java/com/spring/jagalchi/model/CertificateRegCntModel.java
@@ -0,0 +1,197 @@
+package com.spring.jagalchi.model;
+
+public class CertificateRegCntModel {
+	String jmcd; //종목코드
+	String jmnm; //종목이름
+	String piilrcnt1; //올해지원자
+	String piilrcnt2; //필기1년전지원자
+	String piilrcnt3; //필기2년전지원자
+	String piilrcnt4; //필기3년전지원자
+	String piilpcnt1; //필기올해합격자
+	String piilpcnt2; //필기1년전합격자
+	String piilpcnt3; //필기2년전합격자
+	String piilpcnt4; //필기3년전합격자
+	String siilrcnt1; //실기올해지원자
+	String siilrcnt2; //실기1년전지원자
+	String siilrcnt3; //실기2년전지원자
+	String siilrcnt4; //실기3년전지원자
+	String siilpcnt1; //실기올해합격자
+	String siilpcnt2; //실기1년전합격자
+	String siilpcnt3; //실기2년전합격자
+	String siilpcnt4; //실기3년전합격자
+	
+	public CertificateRegCntModel() {	}
+
+	public CertificateRegCntModel(String jmcd, String jmnm, String piilrcnt1, String piilrcnt2, String piilrcnt3,
+			String piilrcnt4, String piilpcnt1, String piilpcnt2, String piilpcnt3, String piilpcnt4) {
+		this.jmcd = jmcd;
+		this.jmnm = jmnm;
+		this.piilrcnt1 = piilrcnt1;
+		this.piilrcnt2 = piilrcnt2;
+		this.piilrcnt3 = piilrcnt3;
+		this.piilrcnt4 = piilrcnt4;
+		this.piilpcnt1 = piilpcnt1;
+		this.piilpcnt2 = piilpcnt2;
+		this.piilpcnt3 = piilpcnt3;
+		this.piilpcnt4 = piilpcnt4;
+	}
+	
+	
+
+	public CertificateRegCntModel(String siilrcnt1, String siilrcnt2, String siilrcnt3, String siilrcnt4,
+			String siilpcnt1, String siilpcnt2, String siilpcnt3, String siilpcnt4) {
+		this.siilrcnt1 = siilrcnt1;
+		this.siilrcnt2 = siilrcnt2;
+		this.siilrcnt3 = siilrcnt3;
+		this.siilrcnt4 = siilrcnt4;
+		this.siilpcnt1 = siilpcnt1;
+		this.siilpcnt2 = siilpcnt2;
+		this.siilpcnt3 = siilpcnt3;
+		this.siilpcnt4 = siilpcnt4;
+	}
+
+	public String getJmcd() {
+		return jmcd;
+	}
+
+	public void setJmcd(String jmcd) {
+		this.jmcd = jmcd;
+	}
+
+	public String getJmnm() {
+		return jmnm;
+	}
+
+	public void setJmnm(String jmnm) {
+		this.jmnm = jmnm;
+	}
+
+	public String getPiilrcnt1() {
+		return piilrcnt1;
+	}
+
+	public void setPiilrcnt1(String piilrcnt1) {
+		this.piilrcnt1 = piilrcnt1;
+	}
+
+	public String getPiilrcnt2() {
+		return piilrcnt2;
+	}
+
+	public void setPiilrcnt2(String piilrcnt2) {
+		this.piilrcnt2 = piilrcnt2;
+	}
+
+	public String getPiilrcnt3() {
+		return piilrcnt3;
+	}
+
+	public void setPiilrcnt3(String piilrcnt3) {
+		this.piilrcnt3 = piilrcnt3;
+	}
+
+	public String getPiilrcnt4() {
+		return piilrcnt4;
+	}
+
+	public void setPiilrcnt4(String piilrcnt4) {
+		this.piilrcnt4 = piilrcnt4;
+	}
+
+	public String getPiilpcnt1() {
+		return piilpcnt1;
+	}
+
+	public void setPiilpcnt1(String piilpcnt1) {
+		this.piilpcnt1 = piilpcnt1;
+	}
+
+	public String getPiilpcnt2() {
+		return piilpcnt2;
+	}
+
+	public void setPiilpcnt2(String piilpcnt2) {
+		this.piilpcnt2 = piilpcnt2;
+	}
+
+	public String getPiilpcnt3() {
+		return piilpcnt3;
+	}
+
+	public void setPiilpcnt3(String piilpcnt3) {
+		this.piilpcnt3 = piilpcnt3;
+	}
+
+	public String getPiilpcnt4() {
+		return piilpcnt4;
+	}
+
+	public void setPiilpcnt4(String piilpcnt4) {
+		this.piilpcnt4 = piilpcnt4;
+	}
+
+	public String getSiilrcnt1() {
+		return siilrcnt1;
+	}
+
+	public void setSiilrcnt1(String siilrcnt1) {
+		this.siilrcnt1 = siilrcnt1;
+	}
+
+	public String getSiilrcnt2() {
+		return siilrcnt2;
+	}
+
+	public void setSiilrcnt2(String siilrcnt2) {
+		this.siilrcnt2 = siilrcnt2;
+	}
+
+	public String getSiilrcnt3() {
+		return siilrcnt3;
+	}
+
+	public void setSiilrcnt3(String siilrcnt3) {
+		this.siilrcnt3 = siilrcnt3;
+	}
+
+	public String getSiilrcnt4() {
+		return siilrcnt4;
+	}
+
+	public void setSiilrcnt4(String siilrcnt4) {
+		this.siilrcnt4 = siilrcnt4;
+	}
+
+	public String getSiilpcnt1() {
+		return siilpcnt1;
+	}
+
+	public void setSiilpcnt1(String siilpcnt1) {
+		this.siilpcnt1 = siilpcnt1;
+	}
+
+	public String getSiilpcnt2() {
+		return siilpcnt2;
+	}
+
+	public void setSiilpcnt2(String siilpcnt2) {
+		this.siilpcnt2 = siilpcnt2;
+	}
+
+	public String getSiilpcnt3() {
+		return siilpcnt3;
+	}
+
+	public void setSiilpcnt3(String siilpcnt3) {
+		this.siilpcnt3 = siilpcnt3;
+	}
+
+	public String getSiilpcnt4() {
+		return siilpcnt4;
+	}
+
+	public void setSiilpcnt4(String siilpcnt4) {
+		this.siilpcnt4 = siilpcnt4;
+	}
+	
+}
---
+++ src/main/java/com/spring/jagalchi/parser/CertificateRegCntParser.java
@@ -0,0 +1,249 @@
+package com.spring.jagalchi.parser;
+//h9Rl76R0OAPihVKvN2LnodougR42HzY43KMDkAZVvqs1eNMFXlAn086yNNz51LBJI7BYo7E2JkWjo6tpgFS1nQ%3D%3D
+
+import java.io.BufferedInputStream;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.spring.jagalchi.model.CertificateRegCntModel;
+
+public class CertificateRegCntParser {
+	//String oracleURL = "jdbc:oracle:thin:@localhost:1521:orcl";
+	String oracleURL = "jdbc:oracle:thin:@localhost:1521:xe";
+	String id="scott";
+	String pass = "tiger";
+
+	Connection conn = null;
+	ArrayList<String> codes = new ArrayList<String>();
+	public CertificateRegCntParser() {
+		try{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("driver load success");
+			conn = DriverManager.getConnection(oracleURL, id, pass);
+		}catch (SQLException e1) {
+			e1.printStackTrace();
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+
+		String sql = "select distinct jmcd from certificate order by jmcd";
+
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+
+			while(rs.next()) {
+				codes.add(rs.getString(1));
+			}
+			rs.close();
+			conn.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}/*
+		for(int i = 0 ; i< codes.size() ; i++) {
+			System.out.println(Integer.toString(Integer.parseInt(codes.get(i))));
+		}*/
+		for(int i = 0 ; i< codes.size() ; i++) {
+			parse(codes.get(i), true);
+			parse(codes.get(i), false);
+		}
+	}
+
+	public void parse(String code, boolean flag) {
+
+		JSONParser jsonparser = new JSONParser();
+		JSONObject jsonobject = null;
+		
+		try {
+			jsonobject = (JSONObject)jsonparser.parse(readUrl(code, flag));
+		} catch (ParseException e2) {
+			e2.printStackTrace();
+		} catch (Exception e2) {
+			e2.printStackTrace();
+		}
+
+		try{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("driver load success");
+			conn = DriverManager.getConnection(oracleURL, id, pass);
+		}catch (SQLException e1) {
+			e1.printStackTrace();
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+		System.out.println((JSONObject)jsonobject.get("response"));
+		JSONObject response = (JSONObject)jsonobject.get("response");
+		JSONObject body = (JSONObject)response.get("body");
+		JSONObject items = null;
+
+		try {
+			items = (JSONObject)body.get("items");
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+			return;
+		}
+
+		JSONArray item = new JSONArray();
+		if (items.get("item") instanceof JSONObject) { 
+			item = new JSONArray();
+			item.add((JSONObject) items.get("item"));
+
+		}
+		else{
+			item = (JSONArray)items.get("item");
+		}
+		System.out.println(code+" : ~~"+item);
+
+		if(flag) {
+			for(int i = 0 ; i < item.size() ; i++) {
+
+				CertificateRegCntModel cm = new CertificateRegCntModel(
+						code, 
+						(((JSONObject)item.get(i)).get("jmnm") == null ? "" : ((JSONObject)item.get(i)).get("jmnm")).toString(), 
+						(((JSONObject)item.get(i)).get("ilrcnt1") == null ? "" : ((JSONObject)item.get(i)).get("ilrcnt1")).toString(), 
+						(((JSONObject)item.get(i)).get("ilrcnt2") == null ? "" : ((JSONObject)item.get(i)).get("ilrcnt2")).toString(), 
+						(((JSONObject)item.get(i)).get("ilrcnt3") == null ? "" : ((JSONObject)item.get(i)).get("ilrcnt3")).toString(), 
+						(((JSONObject)item.get(i)).get("ilrcnt4") == null ? "" : ((JSONObject)item.get(i)).get("ilrcnt4")).toString(), 
+						(((JSONObject)item.get(i)).get("ilpcnt1") == null ? "" : ((JSONObject)item.get(i)).get("ilpcnt1")).toString(), 
+						(((JSONObject)item.get(i)).get("ilpcnt2") == null ? "" : ((JSONObject)item.get(i)).get("ilpcnt2")).toString(), 
+						(((JSONObject)item.get(i)).get("ilpcnt3") == null ? "" : ((JSONObject)item.get(i)).get("ilpcnt3")).toString(), 
+						(((JSONObject)item.get(i)).get("ilpcnt4") == null ? "" : ((JSONObject)item.get(i)).get("ilpcnt4")).toString()
+						);
+				insertCer(cm, code);
+
+				System.out.println(""+i+" "+cm);
+
+			}
+		}else {
+			for(int i = 0 ; i < item.size() ; i++) {
+
+				CertificateRegCntModel cm = new CertificateRegCntModel(
+						(((JSONObject)item.get(i)).get("ilrcnt1") == null ? "" : ((JSONObject)item.get(i)).get("ilrcnt1")).toString(), 
+						(((JSONObject)item.get(i)).get("ilrcnt2") == null ? "" : ((JSONObject)item.get(i)).get("ilrcnt2")).toString(), 
+						(((JSONObject)item.get(i)).get("ilrcnt3") == null ? "" : ((JSONObject)item.get(i)).get("ilrcnt3")).toString(), 
+						(((JSONObject)item.get(i)).get("ilrcnt4") == null ? "" : ((JSONObject)item.get(i)).get("ilrcnt4")).toString(), 
+						(((JSONObject)item.get(i)).get("ilpcnt1") == null ? "" : ((JSONObject)item.get(i)).get("ilpcnt1")).toString(), 
+						(((JSONObject)item.get(i)).get("ilpcnt2") == null ? "" : ((JSONObject)item.get(i)).get("ilpcnt2")).toString(), 
+						(((JSONObject)item.get(i)).get("ilpcnt3") == null ? "" : ((JSONObject)item.get(i)).get("ilpcnt3")).toString(), 
+						(((JSONObject)item.get(i)).get("ilpcnt4") == null ? "" : ((JSONObject)item.get(i)).get("ilpcnt4")).toString()
+						);
+				updateCer(cm, code);
+
+				System.out.println(""+i+" "+cm);
+
+			}
+		}
+		
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+	}
+
+	public void insertCer(CertificateRegCntModel cm, String code) {
+
+
+		String sql = "insert into certificateregcnt(jmcd, jmnm, piilrcnt1, piilrcnt2, piilrcnt3, piilrcnt4, piilpcnt1, piilpcnt2, piilpcnt3, piilpcnt4) values(?,?,?,?,?,?,?,?,?,?)";
+
+		PreparedStatement pstmt = null;
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, cm.getJmcd());
+			pstmt.setString(2, cm.getJmnm());
+			pstmt.setString(3, cm.getPiilrcnt1());
+			pstmt.setString(4, cm.getPiilrcnt2());
+			pstmt.setString(5, cm.getPiilrcnt3());
+			pstmt.setString(6, cm.getPiilrcnt4());
+			pstmt.setString(7, cm.getPiilpcnt1());
+			pstmt.setString(8, cm.getPiilpcnt2());
+			pstmt.setString(9, cm.getPiilpcnt3());
+			pstmt.setString(10, cm.getPiilpcnt4());
+			pstmt.execute();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public void updateCer(CertificateRegCntModel cm, String code) {
+
+		String sql = "update certificateregcnt set siilrcnt1 = ?, siilrcnt2 = ?, siilrcnt3 = ?, siilrcnt4 = ?,"
+										+ "siilpcnt1 = ?, siilpcnt2 = ?, siilpcnt3 = ?, siilpcnt4 = ?"
+										+ "where jmcd = ?";
+		PreparedStatement pstmt = null;
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, cm.getSiilrcnt1());
+			pstmt.setString(2, cm.getSiilrcnt2());
+			pstmt.setString(3, cm.getSiilrcnt3());
+			pstmt.setString(4, cm.getSiilrcnt4());
+			pstmt.setString(5, cm.getSiilpcnt1());
+			pstmt.setString(6, cm.getSiilpcnt2());
+			pstmt.setString(7, cm.getSiilpcnt3());
+			pstmt.setString(8, cm.getSiilpcnt4());
+			pstmt.setString(9, code);
+			pstmt.execute();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	private static String readUrl(String code, boolean flag) throws Exception {
+		BufferedInputStream reader = null;
+		try {
+			URL url = null;
+			if(flag) {
+				url = new URL(
+						"http://openapi.q-net.or.kr/api/service/rest/InquiryStatSVC/getEventYearPiList"
+								+"?_type=json&baseYY=2018&serviceKey=xhuWHchCXW3inWGbrk5MaVf6QjDeeYCFL0xQxkhYGHooJeViPkr%2FAYUmu1W79rr3ljDdrkL7NnL7SQ4Pf9TG%2Fw%3D%3D&jmCd=" + code);
+			}else {
+				url = new URL(
+						"http://openapi.q-net.or.kr/api/service/rest/InquiryStatSVC/getEventYearSiList"
+								+"?_type=json&baseYY=2018&serviceKey=xhuWHchCXW3inWGbrk5MaVf6QjDeeYCFL0xQxkhYGHooJeViPkr%2FAYUmu1W79rr3ljDdrkL7NnL7SQ4Pf9TG%2Fw%3D%3D&jmCd=" + code);
+			}
+
+			reader = new BufferedInputStream(url.openStream());
+			StringBuffer buffer = new StringBuffer();
+			int i;
+			byte[] b = new byte[4096];
+			while( (i = reader.read(b)) != -1){
+				buffer.append(new String(b, 0, i));
+			}
+			return buffer.toString();
+		} finally {
+			if (reader != null)
+				reader.close();
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			CertificateRegCntParser cp = new CertificateRegCntParser();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
